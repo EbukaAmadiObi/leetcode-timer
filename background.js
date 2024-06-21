e3162bdbc9639ab0ae009418f6fd65d14238d11b
ebukaamadiobi <ebukaamadiobi@gmail.com>
2024-06-21 22:11:44 +0100

    background.js: added listener for leetcode page

--- background.js
+++ background.js
@@ -1,11 +1,9 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.includes("leetcode.com/problems") && changeInfo.status === 'complete') {
+    const name = tab.url.split("/problems/")[1];
+    console.log(name);
     chrome.tabs.sendMessage(tabId, {
-      type: "NEW",
-      videoId: urlParameters.get("v"),
+      type: "NEW"
     });
   }
 });
