a05b6f741675ba7301e4caed2c9f3226e9742b5c
ebukaamadiobi <ebukaamadiobi@gmail.com>
2024-06-22 21:39:01 +0100

    Moved countdown function to background.js
    That way it can actually run properly lol. Was refactored to use
    setInterval() and currently outputs time to console.

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url && tab.url.includes("leetcode.com/problems") && changeInfo.status === 'complete') {
-    const name = tab.url.split("/problems/")[1];
-    console.log(name);
-    chrome.tabs.sendMessage(tabId, {
-      type: "NEW"
-    });
+    chrome.action.setPopup({popup: 'popup.html'});
+    console.log(tab.url);
+    countDownFrom(65);  
   }
 });
+
+const countDownFrom = (time) => {
+  console.log("Starting timer...");
+  var now = new Date();
+  var alarmDate = new Date();
+  alarmDate.setMinutes(alarmDate.getMinutes()+time);
+  //twentyMinutesLater.setMinutes(twentyMinutesLater.getMinutes() + 20);
+  console.log("Now is: "+now)
+  console.log("Duration: "+time)
+  console.log("To: "+alarmDate)
+  setInterval(
+    function(){
+      var now = new Date();
+      var Mldistance = alarmDate - now;
+      var distance = new Date(Mldistance)
+      console.log((distance.getHours()-1)+":"+distance.getMinutes()+":"+distance.getSeconds())
+  }, 1000);
+};
